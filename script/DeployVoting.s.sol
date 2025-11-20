1d17acdc392e43a954e512a4d89a1e7e06501ab4
Myles-Traut <myles@linumlabs.com>
2025-11-20 15:42:35 +0200

    Voting: Sepolia Deployment

--- script/DeployVoting.s.sol
+++ script/DeployVoting.s.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.25;
 import {Script} from "forge-std/Script.sol";
 import {Voting} from "../src/Voting.sol";
 
+// Deployed Sepolia Address:  0x0e4897aB9EEf760CF0a8e8bcD31936E35a25cc53
+
 contract DeployVoting is Script {
     function run(address _owner) external returns (Voting) {
         vm.startBroadcast(_owner);
